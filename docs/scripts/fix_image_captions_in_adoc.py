d462802bac502da1fbfa891cea1aa344df4b8e7c
zrgt <garmaev@gmx.de>
2023-07-13 18:44:31 +0200

    Fix the image captions
    - move captions to beginnings of image definitions
    - refactor the script

--- docs/scripts/fix_image_captions_in_adoc.py
+++ docs/scripts/fix_image_captions_in_adoc.py
@@ -1,23 +1,24 @@
 import re
 
 
+# Define the callback function
+def replacement(match):
+    figure_tag = match[1]
+    caption: str = match[2]
+
+    # Move the caption to the beginning of the figure tag
+    modified_figure_tag = f'.{caption.strip()}\n{figure_tag}\n'
+
+    return modified_figure_tag
+
+
 def move_caption_to_beginning(content):
     # Define the regular expression pattern to match figure tags with specific captions
-    pattern = r'(image:\S+\[.*?\])(.*?\n)'
+    pattern = r'(image:\S+\[.*?\])\s+?\n?\n?(Figure.*?\n)'
 
     # Find all occurrences of the pattern in the content
     matches = re.findall(pattern, content)
 
-    # Iterate over the matches and modify the figure tags
-    for match in matches:
-        figure_tag = match[0]
-        caption: str = match[1]
-
-        # Move the caption to the beginning of the figure tag
-        modified_figure_tag = f'.{caption.strip()}\n{figure_tag}\n'
-        old_figure_tag = f'{figure_tag}{caption}'
-
-        # Replace the original figure tag with the modified one
-        content = content.replace(old_figure_tag, modified_figure_tag)
-
-    return content
+    # Replace the original figure tags with the modified ones
+    new_content = re.sub(pattern, replacement, content)
+    return new_content
