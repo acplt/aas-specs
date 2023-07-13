90a36a34c36f77bb17f7ad81c2fd54336cde905f
zrgt <garmaev@gmx.de>
2023-07-13 16:04:03 +0200

    Remove bug refs with "\.anchor"

--- docs/scripts/remove_pattern_from_adoc.py
+++ docs/scripts/remove_pattern_from_adoc.py
@@ -2,8 +2,8 @@ import re
 
 REGULAR_EXP_LIST = [
     r'Table of Contents\n\n(.*?)(?=\n\n==)',
-    r'\[#_Toc\d* \.anchor]####Table \d*',
-    r'\[#_Ref\d* \.anchor]####Table \d*',
+    r'\[#_Toc\d* \.anchor]####Table \d*\s?',
+    r'\[#_Ref\d* \.anchor]####Table \d*\s?',
     r'\[\#_Ref\d* \.anchor\](?:\#{2,4})',
     r'\[\#_Toc\d* \.anchor\](?:\#{2,4})', r'\{empty\}'
 ]
