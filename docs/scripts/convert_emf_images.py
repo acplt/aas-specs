ba6b42e1c8fd81eae139f775eb108e349f77f542
zrgt <garmaev@gmx.de>
2023-08-07 17:54:48 +0200

    Add additional method for image convertion

--- docs/scripts/convert_emf_images.py
+++ docs/scripts/convert_emf_images.py
@@ -1,14 +1,24 @@
 from PIL import Image
 import os
+from wand.image import Image as wima
+
+
+def convert_emf_to_png_with_wand(emf_file: str, png_file):
+    with wima(filename=emf_file) as img:
+        img.format = 'png'
+        img.save(filename=png_file)
 
 
 def convert_emf_to_png(emf_file, png_file):
-    # Open the EMF file
-    im = Image.open(emf_file)
+    try:
+        # Open the EMF file
+        im = Image.open(emf_file)
 
-    # Save it as PNG
-    print(emf_file)
-    im.save(png_file, 'PNG')
+        # Save it as PNG
+        print(emf_file)
+        im.save(png_file, 'PNG')
+    except OSError:
+        convert_emf_to_png_with_wand(emf_file, png_file)
 
 
 def read_emf_images(folder_path):
